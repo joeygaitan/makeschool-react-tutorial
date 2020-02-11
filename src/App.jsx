342c9f1a99457946d552edaecde65563e9b4c126
Joey Gaitan <joeygaitan707@gmail.com>
2020-02-11 10:36:03 -0800

    added in the pageContent

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css';
 
 //components
 import PageHeader from './components/PageHeader';
+import PageContent from './components/PageContent';
 
 //you need to have the react variable to use jsx... the more you know
 function App() {
   return (
     <div className='App'>
       <PageHeader/>
+      <PageContent/>
     </div>
   );
 }
---
+++ src/components/PageContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PageContent = () => {
+    return (
+    <div>
+    <h2>Content here...</h2>
+    </div>
+    )
+   }
+   
+   export default PageContent
