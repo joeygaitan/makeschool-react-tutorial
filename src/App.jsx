bfd3a75239ba15fa4e595b0952c1b141017379a4
Joey Gaitan <joeygaitan707@gmail.com>
2020-02-11 10:33:16 -0800

    added a repo change

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 //components
 import PageHeader from './components/PageHeader';
 
+//you need to have the react variable to use jsx... the more you know
 function App() {
   return (
     <div className='App'>
